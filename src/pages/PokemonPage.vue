ee6e95b350f4aeab38eda670f9bec780e1b32afb
Paola Dl <kath.pao.acgmail.com>
2023-08-17 16:42:35 -0600

    firt commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import type { VueElement } from 'vue';
+
+    import PokemonOptions from '@/components/PokemonOptions.vue'
+    import PokemonPicture from '@/components/PokemonPicture.vue'
+    export default{
+        components:{ PokemonOptions, PokemonPicture}
+        
+
+        
+    }
+    
+</script>
+    
+<template>
+  <h1>¿Quien es este Pokemon?</h1>
+
+  <PokemonPicture :pokemonId='3' :showPokemon="false" />
+  <PokemonOptions />
+</template>
+
+<style>
+ 
+</style>
